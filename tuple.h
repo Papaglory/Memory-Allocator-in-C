503f3e22b13a4cca13c3c84687827760ad026063
Marius Naasen <mariusnaasen@gmail.com>
2024-09-08 18:44:46 +0200

    Added header files for generic linked list, generic node and generic tuple.

---
+++ tuple.h
@@ -0,0 +1,22 @@
+#ifndef TUPLE_H
+#define TUPLE_H
+
+#include <stddef.h>
+
+typedef struct {
+
+    // Reference to the first generic data
+    void* first_data;
+
+    // Reference to the second generic data
+    void* snd_data;
+
+    // Data size of 'first_data'
+    size_t first_size;
+
+    // Data size of 'snd_data'
+    size_t snd_size;
+
+} Tuple
+
+#endif // TUPLE_H
