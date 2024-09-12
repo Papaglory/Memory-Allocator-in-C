6e3c09f44648821e3fed87473d456eae08b891bc
Marius Naasen <mariusnaasen@gmail.com>
2024-09-12 11:31:01 +0200

    add linked_list_tester.c

---
+++ linked_list_tester.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "linked_list.h"
+#include "node.h"
+#include "../iterator/linked_list_iterator.h"
+
+
+
+
+int main() {
+
+    // Allocate linked list
+   LinkedList* list = (LinkedList*) malloc(sizeof(LinkedList));
+
+
+
+
+    // Allocate iterator
+    LinkedListIterator* iterator = (LinkedListIterator*) malloc(sizeof(LinkedListIterator));
+
+
+
+}
