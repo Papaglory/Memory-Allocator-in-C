ae90315d13326da28e7f9704832b363a6e15c667
Marius Naasen <mariusnaasen@gmail.com>
2024-09-16 18:03:00 +0200

    Added function prototypes to memory_triplet.h

--- src/custom_allocator/other_modules/memory_triplet.h
+++ src/custom_allocator/other_modules/memory_triplet.h
@@ -17,6 +17,21 @@ typedef struct {
 
 } MemoryTriplet;
 
-MemoryTriplet
+/*
+* @brief Create a MemoryTriplet and allocate it on the sub-heap.
+*
+* @param1 Pointer to the start of memory block.
+* @param2 Size of the memory block pointed to.
+* @param3 Whether the memory block is free or used.
+* @return A MemoryTriplet object.
+*/
+MemoryTriplet* create_memory_triplet(void* memory_start, size_t block_size, bool free);
+
+/*
+* @brief Destroy a MemoryTriplet object and free it from the sub-heap.
+*
+* @param The MemoryTriplet object to be destroyed and freed.
+*/
+void destroy_memory_triplet(MemoryTriplet* triplet);
 
 #endif // MEMORY_TRIPLET_H
