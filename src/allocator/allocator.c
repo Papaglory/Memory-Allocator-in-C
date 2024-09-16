9ef38729486d274a0534bcf44673794e0d801569
Marius Naasen <mariusnaasen@gmail.com>
2024-09-16 14:10:28 +0200

    Started implementing create_allocator()

--- src/allocator/allocator.c
+++ src/allocator/allocator.c
@@ -4,36 +4,9 @@
 
 Allocator* create_allocator(size_t size) {
 
-    // Allocate memory for the instance
-    Allocator* alloc = (Allocator*) malloc(sizeof(Allocator));
-    if (alloc == NULL) {
+    // Use the built-in C malloc to get the sub-heap
+    void* sub_heap = malloc(size);
 
-        // Allocation failed
-        return NULL;
 
-    }
-
-    // Make member variable point to allocated sub heap
-    alloc->sub_heap = (unsigned char*) malloc(size);
-    if (alloc->sub_heap == NULL) {
-
-        // Allocation failed
-        free(alloc);
-        return NULL;
-
-    }
-
-    // TODO implement
-    return NULL;
-}
-
-void destroy_allocator(Allocator* term) {
-
-    // Free the memory corresponding to sub heap
-    void* sub_heap = term->sub_heap;
-    free(sub_heap);
-
-    // Free TerminatorAlloc object
-    free(term);
 
 }
