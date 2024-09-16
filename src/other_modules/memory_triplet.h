242f02ec265d4ebb0a590c35327fd09d3c783283
Marius Naasen <mariusnaasen@gmail.com>
2024-09-16 16:06:36 +0200

    Progress to memory_triplet.h

--- src/other_modules/memory_triplet.h
+++ src/other_modules/memory_triplet.h
@@ -1,14 +1,22 @@
-#ifndef TRIPLET_H
-#define TRIPLET_H
+#ifndef MEMORY_TRIPLET_H
+#define MEMORY_TRIPLET_H
 
+#include <stdbool.h>
+#include <stddef.h>
 
 typedef struct {
 
+    // Points to the start of a memory block
+    void* memory_start;
 
+    // The size of the memory block
+    size_t block_size;
 
+    // Whether the memory block is free or used
+    bool free;
 
-} Triplet;
+} MemoryTriplet;
 
+MemoryTriplet
 
-
-#endif // TRIPLET_H
+#endif // MEMORY_TRIPLET_H
